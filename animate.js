8e4321d416fc3f2ded13be45a016b9bd05b6389d
Scarleth Estevez <scarleth@bu.edu>
2015-11-03 22:41:47 -0500

    a

--- animate.js
+++ animate.js
@@ -2,20 +2,12 @@ $(window).scroll(function(){
 
 
 	var wScroll= $(this).scrollTop();
-	console.log(wScroll)
+	
 
 	$('.moving-header').css({
 		'transform': 'translate(0px,'+ wScroll /2 + '%)'
 
 		});
 
-	
-	
 
-	
-
-
-
-
-//523
 });
