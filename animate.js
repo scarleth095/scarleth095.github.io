5d2dab043b4f6be41b7df77775e934634bb8cb92
Scarleth Estevez <scarleth@bu.edu>
2015-11-03 20:21:14 -0500

    a

--- animate.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  var deck = new $.scrolldeck({
-    buttons: '.nav-button',
-    slides: '.slide',
-    duration: 600,
-    easing: 'easeInOutExpo',
-	offset: 0
-  });
-});
